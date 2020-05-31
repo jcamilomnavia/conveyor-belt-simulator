b3fef4d44f6d3a4fa3f9393bcbd47bfc2c441fc3
jcamilomnavia <jcamilomnavia@hotmail.com>
2020-05-31 11:10:26 -0500

    Added change color status, fixed position circle

--- banda-python/banda.py
+++ banda-python/banda.py
@@ -7,10 +7,13 @@ import os
 circle = './circle.png'
 conveyer = './banda.png'
 
+principalColor = 'cyan'
+hoverColor = 'red'
+
 windowWidht = 1500
 windowHeight = 500
 
-s1_status = False
+s1_status = True
 # s1_stop = False
 s1_x1 = 1020
 s1_x2 = 1000
@@ -51,9 +54,9 @@ class GFG:
         self.image = ImageTk.PhotoImage(img)
         self.canvas.create_image(windowWidht/4, windowHeight/3,anchor="nw",image = self.image)
 
-        self.rectangle = self.canvas.create_oval(1000, 170, 1045, 215, fill = "yellow")
+        self.rectangle = self.canvas.create_oval(975, 180, 1020, 225, fill = "yellow")
 
-        self.pointS1 = self.canvas.create_oval(1020, 270, 1000, 290, fill = "cyan")
+        self.pointS1 = self.canvas.create_oval(1020, 270, 1000, 290, fill = hoverColor)
         self.pointS2 = self.canvas.create_oval(800, 270, 820, 290, fill = "cyan")
         self.pointS3 = self.canvas.create_oval(600, 270, 620, 290, fill = "cyan")
         self.pointS4 = self.canvas.create_oval(435, 270, 415, 290, fill = "cyan")
@@ -68,6 +71,27 @@ class GFG:
         self.canvas.after(100, self.movement)
         self.checkStatus()
         self.checkSerial()
+        self.checkColor()
+
+    def checkColor(self):
+        global s1_status, s2_status, s3_status, s4_status
+        if s1_status == True:
+            self.canvas.itemconfig(self.pointS1, fill=hoverColor)
+        else:
+            self.canvas.itemconfig(self.pointS1, fill=principalColor)
+        if s2_status == True:
+            self.canvas.itemconfig(self.pointS2, fill=hoverColor)
+        else:
+            self.canvas.itemconfig(self.pointS2, fill=principalColor)
+        if s3_status == True:
+            self.canvas.itemconfig(self.pointS3, fill=hoverColor)
+        else:
+            self.canvas.itemconfig(self.pointS3, fill=principalColor)
+        if s4_status == True:
+            self.canvas.itemconfig(self.pointS4, fill=hoverColor)
+        else:
+            self.canvas.itemconfig(self.pointS4, fill=principalColor)
+        
 
     def checkSerial(self):
         x1,y1,x2,y2 = self.canvas.coords(self.rectangle)
@@ -80,6 +104,7 @@ class GFG:
             if p == b'S':
                 self.stop(self)
             if p == b'1':
+                s3_stop = s2_stop = False
                 self.right(self)
             if p == b'2':
                 s2_stop = True
@@ -98,6 +123,7 @@ class GFG:
                 if x1 < s3_x1:
                     self.right(self)
             if p == b'4':
+                s3_stop = s2_stop = False
                 self.left(self)
 
     def checkStatus(self):
@@ -106,12 +132,15 @@ class GFG:
         global s2_stop, s3_stop
 
 
-        if x1 > s1_x1:
+        if x1 < s1_x1 and x2 > s1_x2:
             if s1_status ==  False:
                 s1_status = True
                 s2_status = s3_status = s4_status = False
                 arduino.write(str.encode('Passing by S1 \r'))
-            self.x = 0
+            if x2 == s1_x1:
+                self.x = 0
+        else:
+            s1_status = False
 
 
         if x1 < s2_x1 and x2 > s2_x2:
@@ -121,7 +150,8 @@ class GFG:
                 s2_status = True
                 s1_status = s3_status = s4_status = False
                 arduino.write(str.encode('Passing by S2 \r'))
-
+        else:
+            s2_status = False
 
         if x1 < s3_x1 and x2 > s3_x2:
             if s3_stop:
@@ -130,7 +160,8 @@ class GFG:
                 s3_status = True
                 s2_status = s1_status = s4_status = False
                 arduino.write(str.encode('Passing by S3 \r'))
-
+        else:
+            s3_status = False
 
         if x1 == s4_x2:
             if s4_status ==  False:
@@ -138,6 +169,8 @@ class GFG:
                 s2_status = s3_status = s1_status = False
                 arduino.write(str.encode('Passing by S4 & ending \r'))
             self.stop(self)
+        else:
+            s4_status = False
 
 
     def left(self,event): 
